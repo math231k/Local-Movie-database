b85acadf2f5fd8380cd7358f6f48c5120f887008
rizvan99 <rizvan2312@hotmail.com>
2020-01-06 10:33:49 +0100

    User interface

--- LocalMovieDatabase/src/localmoviedatabase/FXMLDocumentController.java
+++ LocalMovieDatabase/src/localmoviedatabase/FXMLDocumentController.java
@@ -11,6 +11,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,18 +20,59 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TableColumn<?, ?> movieTitle;
+    @FXML
+    private TableColumn<?, ?> movieRating;
+    @FXML
+    private TableColumn<?, ?> categoryName;
+    @FXML
+    private TextField showTitle;
+    @FXML
+    private TextField showCategory;
+    @FXML
+    private TextField showRating;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void addCategory(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void removeCategory(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void addMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void removeMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void editMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void playSelectedMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void editCategory(ActionEvent event)
+    {
+    }
     
 }
